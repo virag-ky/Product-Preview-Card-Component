6ba8b317e453a78942320c1e55bf0fc83c6da5f6
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-08 18:26:03 +0300

    Add Attribution component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import './App.css';
 import Card from './Card';
+import Attribution from './Attribution';
 
 const App = () => {
   return (
     <div className="App">
       <Card />
-      <div className="attribution">
-        <p>
-          Challenge by{' '}
-          <a
-            href="https://www.frontendmentor.io?ref=challenge"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Frontend Mentor
-          </a>
-          .
-        </p>
-        <p>
-          Coded by{' '}
-          <a
-            href="https://github.com/virag-ky"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Virag Kormoczy
-          </a>
-          .
-        </p>
-      </div>
+      <Attribution />
     </div>
   );
 };
