82b5f3a6343c175749ead1ebaa28e72ee67da30e
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-08 17:21:38 +0300

    Add mobile version

--- src/App.js
+++ src/App.js
@@ -6,23 +6,28 @@ const App = () => {
     <div className="App">
       <Card />
       <div className="attribution">
-        Challenge by{' '}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by{' '}
-        <a
-          href="https://github.com/virag-ky"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Virag Kormoczy
-        </a>
-        .
+        <p>
+          Challenge by{' '}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          .
+        </p>
+        <p>
+          Coded by{' '}
+          <a
+            href="https://github.com/virag-ky"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Virag Kormoczy
+          </a>
+          .
+        </p>
       </div>
     </div>
   );
